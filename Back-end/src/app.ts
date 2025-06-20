cf6b23d7e879f0ee9fa7ac62ce4dbb8f250ddff1
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2025-06-20 22:09:24 +0000

    correção da nomeação de arquivos

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { sql } from "./DB/db"; // importa o banco de dados
+import { sql } from "./DB/Database"; // importa o banco de dados
 
 const app = express();
 
