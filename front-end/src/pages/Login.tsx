272e6463767cb9d27b79d3fb16782e19f42fab34
Vinícius Madureira Godinho de Oliveira <vinimagod@gmail.com>
2025-06-21 15:06:59 -0300

    arrumando o login

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -6,7 +6,7 @@ const Login: React.FC = () => {
             <div className="login__container">
 
                 <div className="wellcome__container">
-                    <h1>Bem Vindo(a) ao ToTask</h1>
+                    <h1>Bem vindo(a) ao ToTask</h1>
                     <img className="kanban__image" src="./kanban.svg" alt="" />
                 </div>
                 <div className="login-form__container">
@@ -15,17 +15,17 @@ const Login: React.FC = () => {
                     {/* Username */}
                         <form action="./pages/Login" method="post"></form>
                         <div  className="login-input__box">
-                            <label htmlFor="login">E-mail </label>
+                            <label htmlFor="login">E-mail: </label>
                             <input type="text" id="login" name="login" required />
                         </div>
                     {/* Senha */}
                         <div className="login-input__box">
-                            <label htmlFor="password">Senha </label>
+                            <label htmlFor="password">Senha: </label>
                             <input type="password" id="password" name="password" required />
                         </div>
                     </div>
-                    <div>
-                        <button id="login__button" type="submit">Entrar</button>
+                    <div className="login-button__container">
+                        <button className="login-button" type="submit">Entrar</button>
                         <a className="forgot-password">Esqueci a senha</a>
                     </div>
                 </div>
